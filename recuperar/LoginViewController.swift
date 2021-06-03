b1231c9c110468003ed79e7ce0fba7fd962e5003
jc <jccj1994@gmail.com>
2021-06-03 11:12:06 -0400

    otro avance

--- recuperar/LoginViewController.swift
+++ recuperar/LoginViewController.swift
@@ -9,4 +9,10 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    /*
+    @IBAction func tapToClose(_ sender: Any) {
+        self.view.endEditing(true)
+    }
+     tan solo programar esto hace que crashee .-.
+    */
 }
